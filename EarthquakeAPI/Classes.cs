9d77454ce2bd56333f0d97a0f527ab34ec7d1bf9
Рахимов Серик Балтабаевич <@corp.itstep>
2019-05-30 22:30:19 +0600

    first commit

---
+++ EarthquakeAPI/Classes.cs
@@ -0,0 +1,177 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarthquakeAPI
+{
+    public class Metadata
+    {
+
+        [JsonProperty("generated")]
+        public long Generated { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("status")]
+        public int Status { get; set; }
+
+        [JsonProperty("api")]
+        public string Api { get; set; }
+
+        [JsonProperty("limit")]
+        public int Limit { get; set; }
+
+        [JsonProperty("offset")]
+        public int Offset { get; set; }
+
+        [JsonProperty("count")]
+        public int Count { get; set; }
+    }
+
+    [JsonObject("Properties")]
+    public class Propertis
+    {
+
+        [JsonProperty("mag")]
+        public double Mag { get; set; }
+
+        [JsonProperty("place")]
+        public string Place { get; set; }
+
+        [JsonProperty("time")]
+        public long Time { get; set; }
+
+        [JsonProperty("updated")]
+        public object Updated { get; set; }
+
+        [JsonProperty("tz")]
+        public int Tz { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("detail")]
+        public string Detail { get; set; }
+
+        [JsonProperty("felt")]
+        public object Felt { get; set; }
+
+        [JsonProperty("cdi")]
+        public object Cdi { get; set; }
+
+        [JsonProperty("mmi")]
+        public object Mmi { get; set; }
+
+        [JsonProperty("alert")]
+        public object Alert { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("tsunami")]
+        public int Tsunami { get; set; }
+
+        [JsonProperty("sig")]
+        public int Sig { get; set; }
+
+        [JsonProperty("net")]
+        public string Net { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("ids")]
+        public string Ids { get; set; }
+
+        [JsonProperty("sources")]
+        public string Sources { get; set; }
+
+        [JsonProperty("types")]
+        public string Types { get; set; }
+
+        [JsonProperty("nst")]
+        public int? Nst { get; set; }
+
+        [JsonProperty("dmin")]
+        public double? Dmin { get; set; }
+
+        [JsonProperty("rms")]
+        public double Rms { get; set; }
+
+        [JsonProperty("gap")]
+        public int? Gap { get; set; }
+
+        [JsonProperty("magType")]
+        public string MagType { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+
+    public class Geometry
+    {
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("coordinates")]
+        public IList<double> Coordinates { get; set; }
+    }
+
+    public class Feature
+    {
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("properties")]
+        public Propertis Properties { get; set; }
+
+        [JsonProperty("geometry")]
+        public Geometry Geometry { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+
+    }
+
+    public class RootObject
+    {
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("metadata")]
+        public Metadata Metadata { get; set; }
+
+        [JsonProperty("features")]
+        public IList<Feature> Features { get; set; }
+
+        [JsonProperty("bbox")]
+        public IList<double> Bbox { get; set; }
+    }
+
+    public class Info
+    {
+
+        public string Date { get; set; }
+
+        public string Place { get; set; }
+
+        public string Magnituda { get; set; }
+
+        public string Glubina { get; set; }
+    }
+
+}
---
+++ EarthquakeAPI/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+
+namespace EarthquakeAPI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void InfoLoad(string cnt)
+        {
+            string date;
+            string place;
+            string magnituda;
+            string glubina;
+
+            //          PrBar.Visibility = Visibility.Visible;
+            WebRequest req = WebRequest.Create(@"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time-asc&limit=" + cnt);
+
+            string jsonText = "";
+
+            WebResponse response = req.GetResponse();
+            using (Stream s = response.GetResponseStream()) //Пишем в поток.
+            {
+                using (StreamReader r = new StreamReader(s)) //Читаем из потока.
+                {
+                    jsonText = r.ReadToEnd();
+                }
+            }
+            response.Close(); //Закрываем поток
+
+            //            List<Feature> = JsonConvert.DeserializeObject<List<Feature>>(jsonText);
+
+
+
+            //        eathAll = Feature.FromJson(jsonText).ToList();
+
+            RootObject newpeople = JsonConvert.DeserializeObject<RootObject>(jsonText);
+
+           List<Feature> featureAll = newpeople.Features.ToList();
+
+
+
+
+            //ListData.Items.Clear();
+
+            List<Info> InfoAll = new List<Info>();
+
+            foreach (var t in featureAll)
+            {
+                //var u = DateTimeOffset.FromUnixTimeSeconds(t.Properties.Time);
+                
+                date = t.Properties.Time.ToString();
+               // date = u.ToString();
+               place = t.Properties.Place;
+                magnituda = t.Properties.Mag.ToString();
+                glubina = t.Properties.Sig.ToString();
+
+                InfoAll.Add(new Info
+                {
+                    Date = date,
+                    Place = place,
+                    Magnituda = magnituda,
+                    Glubina = glubina
+                });
+
+            }
+            infoDataGrid.ItemsSource = InfoAll;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int cnt = int.Parse(textCount.Text);
+            }
+            catch 
+            {
+                MessageBox.Show("Введите число");
+                return;
+            }
+            InfoLoad(textCount.Text);
+
+        }
+    }
+}
